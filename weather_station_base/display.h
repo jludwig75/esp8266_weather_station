a443e845d890b70b87c5ac219259c7036f11d4bc
Jonathan Ludwig <jr.ludwig@gmail.com>
2016-10-21 17:59:33 -0600

    Added LCD display management

--- weather_station_base/display.h
+++ weather_station_base/display.h
@@ -9,6 +9,14 @@ class Adafruit_ILI9341;
 class Display
 {
 public:
+	struct Region
+	{
+		int16_t x;
+		int16_t y;
+		int16_t w;
+		int16_t h;
+
+	};
     Display(int8_t _CS, int8_t _DC, int8_t _RST = -1);
 
     void update_time_string(const String & time_string);        // 5:09
@@ -16,4 +24,14 @@ public:
     void update_date_string(const String & date_string);        // Wed, Oct 21, 2016
     void update_inside_temp_string(const String & temp_string); // 72\xA7 F 42% RH
     void update_outside_temp_string(const String & temp_string);// 79\xA7 F 48% RH
+protected:
+	void update_region(const Region &region, const String &str);
+	void clear_region(const Region &region);
+	void write_region(const Region &region, const String &str);
+private:
+	String m_last_time_string;
+	String m_last_time_meridian;
+	String m_last_date_string;
+	String m_last_inside_temp_string;
+	String m_last_outside_temp_string;
 };
--- weather_station_base_sim/display_sim.cpp
+++ weather_station_base_sim/display_sim.cpp
@@ -1,5 +1,11 @@
 #include "display.h"
 
+Display::Region time_region = { 1, 1, 5, 1 };
+Display::Region time_meridian_region = { 6, 1, 2, 1 };
+Display::Region date_region = { 1, 2, 20, 1 };
+Display::Region inside_temp_region = { 1, 3, 20, 1 };
+Display::Region outside_temp_region = { 1, 4, 20, 1 };
+
 
 Display::Display(int8_t _CS, int8_t _DC, int8_t _RST)
 {
@@ -8,29 +14,73 @@ Display::Display(int8_t _CS, int8_t _DC, int8_t _RST)
 
 void Display::update_time_string(const String & time_string)
 {
-    Serial.print(time_string);
+	if (time_string != m_last_time_string)
+	{
+		update_region(time_region, time_string);
+		m_last_time_string = time_string;
+	}
 }
 
 void Display::update_time_meridian(const String & time_meridian)
 {
-    Serial.print(" ");
-    Serial.println(time_meridian);
+	if (time_meridian != m_last_time_meridian)
+	{
+		update_region(time_meridian_region, time_meridian);
+		m_last_time_meridian = time_meridian;
+	}
 }
 
 void Display::update_date_string(const String & date_string)
 {
-    Serial.println(date_string);
+	if (date_string != m_last_date_string)
+	{
+		update_region(date_region, date_string);
+		m_last_date_string = date_string;
+	}
 }
 
 void Display::update_inside_temp_string(const String & temp_string)
 {
-    Serial.print("In: ");
-    Serial.println(temp_string);
+	if (temp_string != m_last_inside_temp_string)
+	{
+		String inside_temp_string = String("In: ") + temp_string;
+		update_region(inside_temp_region, inside_temp_string);
+		m_last_inside_temp_string = temp_string;
+	}
 }
 
 void Display::update_outside_temp_string(const String & temp_string)
 {
-    Serial.print("Out: ");
-    Serial.println(temp_string);
+	if (temp_string != m_last_outside_temp_string)
+	{
+		String outside_temp_string = String("Out: ") + temp_string;
+		update_region(outside_temp_region, outside_temp_string);
+		m_last_outside_temp_string = temp_string;
+	}
 }
 
+
+void Display::update_region(const Region &region, const String &str)
+{
+	clear_region(region);
+	write_region(region, str);
+}
+
+void Display::clear_region(const Region &region)
+{
+	Serial.print("Clearing region @ (");
+	Serial.print(region.x);
+	Serial.print(", ");
+	Serial.print(region.y);
+	Serial.println(")");
+}
+
+void Display::write_region(const Region &region, const String &str)
+{
+	Serial.print("Writing region @ (");
+	Serial.print(region.x);
+	Serial.print(", ");
+	Serial.print(region.y);
+	Serial.print("): ");
+	Serial.println(str);
+}
